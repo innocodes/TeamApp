fadea31d52b1f337399ce0e93aa0095fcc4e2a62
innocodes <oyebodeinnocent@gmail.com>
2023-06-28 14:02:44 +0100

    Complete Name adding feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TeamApp';
+
+  // State management
+  newMemberName = '';
+  members: string[] = [];
+
+  // Handling error
+  errorMessage  = '';
+
+  // Update Name
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName)
+  }
+
+  // Event handling
+  addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = "Name can't be empty"
+      return;
+    } else {
+      this.errorMessage = "";
+      this.members.push(this.newMemberName);
+      this.newMemberName = "";
+      console.log(this.members);
+    }
+  }
+
+
 }
