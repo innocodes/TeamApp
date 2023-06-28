44bff64ab309d6bcb8a496d63280d304c0e05302
innocodes <oyebodeinnocent@gmail.com>
2023-06-28 15:53:00 +0100

    Complete Team Generators logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ export class AppComponent {
   // State management
   newMemberName = '';
   members: string[] = [];
+  numberOfTeams: number | '' = '';
+  teams: string[][] = [];
 
   // Handling error
   errorMessage  = '';
@@ -19,6 +21,11 @@ export class AppComponent {
   onInput(member: string) {
     this.newMemberName = member;
     console.log(this.newMemberName)
+  }
+
+  // Update Number of Teams
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value);
   }
 
   // Event handling
@@ -34,5 +41,25 @@ export class AppComponent {
     }
   }
 
+  generateTeams() {
+    
+    if(!this.numberOfTeams || this.numberOfTeams <= 0) return;
 
+    const allMembers = [...this.members];
+
+    while(allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random() * allMembers.length)
+        const member = allMembers.splice(randomIndex, 1)[0];
+
+        if(!member) break;
+  
+        if(this.teams[i]) {
+          this.teams[i].push(member)
+        } else {
+          this.teams[i] = [member]
+        }
+      }
+    }
+  }
 }
